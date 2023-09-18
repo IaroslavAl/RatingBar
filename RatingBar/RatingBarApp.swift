2b4e14e7d822c24f04cdb714e1178865dc565e20
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-09-18 09:49:22 +0300

    Initial commit

---
+++ RatingBar/RatingBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingBarApp.swift
+//  RatingBar
+//
+//  Created by BELDIN Yaroslav on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct RatingBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
